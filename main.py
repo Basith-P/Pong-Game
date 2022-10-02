fa81df2e4de641190c7cbfb00e40acb221f14c02
Basith P <basithpofficial@gmail.com>
2022-10-02 11:05:40 +0530

    Add scoreboard

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -10,9 +11,10 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-l_paddle = Paddle(-350, 0)
-r_paddle = Paddle(350, 0)
+l_paddle = Paddle(-370, 0)
+r_paddle = Paddle(370, 0)
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -26,12 +28,22 @@ is_game_over = False
 while not is_game_over:
     screen.update()
     ball.move()
-    time.sleep(.1)
+    time.sleep(.08)
 
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
     if (ball.distance(l_paddle) < 50 and ball.xcor() < -290) or (ball.distance(r_paddle) < 50 and ball.xcor() > 290):
         ball.bounce_x()
+
+    if (ball.xcor() < -400):
+        ball.reset()
+        ball.bounce_x()
+        scoreboard.l_point()
+
+    if (ball.xcor() > 400):
+        ball.reset()
+        ball.bounce_x()
+        scoreboard.r_point()
 
 screen.exitonclick()
